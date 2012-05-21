ce4c7975117f3a5820ad8b91158b9fc4beed7556
mathslinux <riegamaths@gmail.com>
2012-05-21 23:32:25 +0800

    ui: Initialize UI subdirectory
    Signed-off-by: mathslinux <riegamaths@gmail.com>

---
+++ src/ui/main.c
@@ -0,0 +1,14 @@
+/**
+ * @file   main.c
+ * @author mathslinux <riegamaths@gmail.com>
+ * @date   Mon May 21 23:29:20 2012
+ * 
+ * @brief  
+ * 
+ * 
+ */
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
