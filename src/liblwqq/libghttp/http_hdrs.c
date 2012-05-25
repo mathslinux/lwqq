6581fa47bb4c8ae61a65b853d269f47b32ca7af0
mathslinux <riegamaths@gmail.com>
2012-05-26 01:07:36 +0800

    liblwqq/http: Fix set http header more than once
    If the header entry is "Set-Cookie", we should add it to headers,
    But for other entry, there is have only one "key-value".
    
    e.g.:
    Host: www.google.com (We can only set one Host)
    Set-Cookie: a=b; c=d; (But we can set more than one)
    Set-Cookie: f=g; ds=sf;
    
    Signed-off-by: mathslinux <riegamaths@gmail.com>

--- src/liblwqq/libghttp/http_hdrs.c
+++ src/liblwqq/libghttp/http_hdrs.c
@@ -236,6 +236,47 @@ http_hdr_set_value_no_nts(http_hdr_list *a_list,
     
 }
 
+/** 
+ * NB: if the key of header is "cookie", things to be more complex,
+ * cause cookie may be more than one.
+ * like:
+ * Set-Cookie: a=b; c=d;
+ * Set-Cookie: e=f; g=m;
+ * So, we set cookie here
+ * 
+ * @param a_list 
+ * @param a_name 
+ * @param a_val 
+ * 
+ * @return 
+ */
+static int
+http_hdr_set_cookie_value(http_hdr_list *a_list,
+                   const char *a_val)
+{
+    int i = 0;
+    char *l_temp_value = NULL;
+    int l_return = 0;
+  
+    for (i = 0; i < HTTP_HDRS_MAX; i++) {
+        if (a_list->header[i])
+            continue;
+        
+        /* I promise not to mess with this value. */
+        l_temp_value = (char *)http_hdr_is_known("Set-Cookie");
+        if (l_temp_value) {
+            a_list->header[i] = l_temp_value;
+            /* dont free this later... */
+        } else{
+            a_list->header[i] = strdup("Set-Cookie");
+        }
+        a_list->value[i] = strdup(a_val);
+        l_return = 1;
+        break;
+	}
+    return l_return;
+}
+
 int
 http_hdr_set_value(http_hdr_list *a_list,
                    const char *a_name,
@@ -244,26 +285,51 @@ http_hdr_set_value(http_hdr_list *a_list,
     int i = 0;
     char *l_temp_value = NULL;
     int l_return = 0;
-  
-    if ((a_list == NULL) || (a_name == NULL) || (a_val == NULL)) {
-        return l_return;
+
+    if ((a_list == NULL) || (a_name == NULL) || (a_val == NULL))
+        goto ec;
+
+    /* If the key is "Set-Cookie", go to a different branch */
+    if (!strcmp(a_name, "Set-Cookie")) {
+        return http_hdr_set_cookie_value(a_list, a_val);
     }
-    for (i = 0; i < HTTP_HDRS_MAX; i++) {
-        if (a_list->header[i])
-            continue;
-        
-        /* I promise not to mess with this value. */
-        l_temp_value = (char *)http_hdr_is_known(a_name);
-        if (l_temp_value) {
-            a_list->header[i] = l_temp_value;
-            /* dont free this later... */
-        } else{
-            a_list->header[i] = strdup(a_name);
+    
+    l_temp_value = http_hdr_get_value(a_list, a_name);
+    if (l_temp_value == NULL)
+    {
+        for (i=0; i < HTTP_HDRS_MAX; i++)
+        {
+            if (a_list->header[i] == NULL)
+            {
+                /* I promise not to mess with this value. */
+                l_temp_value = (char *)http_hdr_is_known(a_name);
+                if (l_temp_value)
+                {
+                    a_list->header[i] = l_temp_value;
+                    /* dont free this later... */
+                }
+                else
+                    a_list->header[i] = strdup(a_name);
+                a_list->value[i] = strdup(a_val);
+                l_return = 1;
+                break;
+            }
         }
-        a_list->value[i] = strdup(a_val);
-        l_return = 1;
-        break;
-	}
+    }
+    else
+    {
+        for(i = 0; i < HTTP_HDRS_MAX; i++)
+        {
+            if (a_list->value[i] == l_temp_value)
+            {
+                free(a_list->value[i]);
+                a_list->value[i] = strdup(a_val);
+                l_return = 1;
+                break;
+            }
+        }
+    }
+ec:
     return l_return;
 }
 
