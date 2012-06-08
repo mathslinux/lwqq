e745b43e91fbd3e8bffbf8fe339a8be84fcc3d3b
mathslinux <riegamaths@gmail.com>
2012-06-08 17:13:08 +0800

    lwdb: Introduction LwdbGlobalDB API
    These APIs provide interface that handle global database
    
    Signed-off-by: mathslinux <riegamaths@gmail.com>

--- src/lwdb/lwdb.c
+++ src/lwdb/lwdb.c
@@ -16,20 +16,37 @@
 
 #include <stdlib.h>
 #include <sqlite3.h>
+#include <unistd.h>
+#include "smemory.h"
 #include "logger.h"
+#include "swsqlite.h"
+#include "lwdb.h"
 
-static const char *global_db_sql =
-    "create table if not exists config("
+LwqqErrorCode lwdb_globaldb_add_new_user(struct LwdbGlobalDB *db,
+                                         const char *qq_number);
+    
+#ifndef LWQQ_CONFIG_DIR
+#define LWQQ_CONFIG_DIR "~/.config/lwqq"
+#endif
+
+#define LWDB_INIT_VERSION 1001
+
+static const char *create_global_db_sql =
+    "create table if not exists configs("
     "    id integer primary key asc autoincrement,family,key,value);"
-    "create table if not exists user("
+    "create table if not exists users("
     "    qqnumber primary key,database_name,password,status,rempwd);";
 
-static const char *user_db_sql =
+#if 0
+static const char *create_user_db_sql =
     "create table if not exists buddies("
-    "     primary key,database_name);";
+    "    qqnumber primary key,category,vip_info,nick,markname,face,flag);"
+    "create table if not exists categories("
+    "    name primary key,index,sort);";
+#endif 
 
 /** 
- * Create user's database
+ * Create database for lwqq
  * 
  * @param filename 
  * @param db_type Type of database you want to create. 0 means
@@ -37,42 +54,156 @@ static const char *user_db_sql =
  * 
  * @return 0 if everything is ok, else return -1
  */
-int lwdb_create_user_db(const char *filename, int db_type)
+int lwdb_create_db(const char *filename, int db_type)
 {
-    sqlite3 *db;
-    char *err = NULL;
     int ret;
     
     if (!filename) {
         return -1;
     }
 
-    ret = sqlite3_open(filename, &db);
-    if (db != SQLITE_OK) {
-        goto failed;
+    if (access(filename, F_OK) == 0) {
+        unlink(filename);
+    }
+    if (db_type == 0) {
+        ret = sws_exec_sql_directly(filename, create_global_db_sql, NULL);
+    } else if (db_type == 1) {
+        ret = sws_exec_sql_directly(filename, create_global_db_sql, NULL);
+    } else {
+        ret = -1;
     }
 
-    if (db_type == 0) {
-        ret = sqlite3_exec(db, global_db_sql, NULL, NULL, &err);
-    } else if (db_type == 1) {
-        ret = sqlite3_exec(db, user_db_sql, NULL, NULL, &err);
-    } else {
-        lwqq_log(LOG_ERROR, "No such database type\n");
-        goto failed;
+    if (ret == 0) {
+        /* Update database version */
+        ret = sws_exec_sql_directly(
+            filename,
+            "INSERT INTO configs (family,key,value) "
+            "VALUES('lwqq','version',1001);", NULL);
     }
     
-    if (ret != SQLITE_OK) {
+    return ret;
+}
+
+static int file_is_global_db(const char *filename)
+{
+    int ret;
+    SwsStmt *stmt = NULL;
+    char *test_sql = "SELECT value FROM config WHERE key='version'";
+    char value[32] = {0};
+
+    
+    /* Open DB */
+    SwsDB *db = sws_open_db(filename, NULL);
+    if (!db) {
+        goto invalid;
+    }
+    
+    /* Query DB */
+    ret = sws_query_start(db, test_sql, &stmt, NULL);
+    if (ret) {
+        goto invalid;
+    }
+    if (sws_query_next(stmt, NULL)) {
+        goto invalid;
+    }
+    sws_query_column(stmt, 0, value, sizeof(value), NULL);
+    sws_query_end(stmt, NULL);
+
+    if (atoi(value) < 1000) {
+        goto invalid;
+    }
+    
+    /* Close DB */
+    sws_close_db(db, NULL);
+
+    return 1;
+    
+invalid:
+    sws_close_db(db, NULL);
+    return 0;
+}
+
+/** 
+ * Create a global DB object
+ * 
+ * @param filename The database filename
+ * @param err Used to store error code
+ * 
+ * @return A new global DB object, or NULL if somethins wrong, and store
+ * error code in err
+ */
+
+LwdbGlobalDB *lwdb_globaldb_new(const char *filename, LwqqErrorCode *err)
+{
+    LwdbGlobalDB *db = NULL;
+    int ret;
+    
+    if (!filename) {
+        if (*err)
+            *err = LWQQ_EC_NULL_POINTER;
+        return NULL;
+    }
+
+    if (!file_is_global_db(filename)) {
+        ret = lwdb_create_db(filename, 0);
+        if (ret) {
+            goto failed;
+        }
+    }
+
+    db = s_malloc0(sizeof(*db));
+    db->db = sws_open_db(filename, NULL);
+    if (!db->db) {
         goto failed;
     }
-
-    sqlite3_close(db);
-    return 0;
+    db->add_new_user = lwdb_globaldb_add_new_user;
+    
+    return db;
 
 failed:
-    lwqq_log(LOG_ERROR, "Create global database failed\n");
-    if (err) {
-        sqlite3_free(err);
+    lwdb_globaldb_free(db);
+    return NULL;
+}
+
+/** 
+ * Free a LwdbGlobalDb object
+ * 
+ * @param db 
+ */
+void lwdb_globaldb_free(LwdbGlobalDB *db)
+{
+    if (db) {
+        sws_close_db(db->db, NULL);
+        s_free(db);
     }
-    sqlite3_close(db);
-    return -1;
+}
+
+/** 
+ * 
+ * 
+ * @param db 
+ * @param qq_number 
+ * 
+ * @return LWQQ_EC_OK on success, else return LWQQ_EC_DB_EXEC_FAIELD on failure
+ */
+LwqqErrorCode lwdb_globaldb_add_new_user(struct LwdbGlobalDB *db,
+                                         const char *qq_number)
+{
+    char *errmsg = NULL;
+    char sql[256];
+
+    if (!qq_number){
+        return LWQQ_EC_NULL_POINTER;
+    }
+    
+    snprintf(sql, sizeof(sql), "INSERT INTO users (qqnumber) VALUES('%s');",
+             qq_number);
+    sws_exec_sql(db->db, sql, &errmsg);
+    if (errmsg) {
+        lwqq_log(LOG_ERROR, "Add new user error: %s\n", errmsg);
+        s_free(errmsg);
+        return LWQQ_EC_DB_EXEC_FAIELD;
+    }
+
+    return LWQQ_EC_OK;
 }
--- src/lwdb/lwdb.h
+++ src/lwdb/lwdb.h
@@ -12,15 +12,72 @@
 #ifndef LWDB_H
 #define LWDB_H
 
+#include "type.h"
+#include "swsqlite.h"
+
+/************************************************************************/
+/* LwdbGlobalDB API */
+
+typedef struct LwdbGlobalDB {
+    SwsDB *db;                  /**< Pointer sqlite3 db */
+    LwqqErrorCode (*add_new_user)(struct LwdbGlobalDB *db, const char *qq_number);
+} LwdbGlobalDB;
+
 /** 
- * Create user's database
+ * Create a global DB object
+ * 
+ * @param filename The database filename
+ * @param err Used to store error code
+ * 
+ * @return A new global DB object, or NULL if somethins wrong, and store
+ * error code in err
+ */
+LwdbGlobalDB *lwdb_globaldb_new(const char *filename, LwqqErrorCode *err);
+
+/** 
+ * Free a LwdbGlobalDb object
+ * 
+ * @param db 
+ */
+void lwdb_globaldb_free(LwdbGlobalDB *db);
+
+/* LwdbGlobalDB API end */
+
+/************************************************************************/
+/* LwdbUserDB API */
+
+typedef struct LwdbUserDB {
+} LwdbUserDB;
+/** 
+ * Create a user DB object
+ * 
+ * @param filename The database filename
+ * @param err Used to store error code
+ * 
+ * @return A new user DB object, or NULL if somethins wrong, and store
+ * error code in err
+ */
+LwdbUserDB *lwdb_userdb_new(const char *filename, LwqqErrorCode *err);
+
+/** 
+ * Free a LwdbUserDB object
+ * 
+ * @param db 
+ */
+void lwdb_userdb_free(LwdbUserDB *db);
+
+/* LwdbUserDB API end */
+
+/************************************************************************/
+/** 
+ * Create database for lwqq
  * 
  * @param filename 
  * @param db_type Type of database you want to create. 0 means
  *        global database, 1 means user database
- *
+ * 
  * @return 0 if everything is ok, else return -1
  */
-int lwdb_create_user_db(const char *filename, int db_type);
+int lwdb_create_db(const char *filename, int db_type);
 
 #endif
