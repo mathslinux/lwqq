aa277a2122781af9060f614e958be2692978453f
mathslinux <riegamaths@gmail.com>
2012-06-09 11:11:40 +0800

    autogen.sh: After excute autoconf, excute configure
    Signed-off-by: mathslinux <riegamaths@gmail.com>

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -x
 aclocal 					\
-&& autoconf					\
-&& libtoolize 	--copy --force --automake 	\
-&& autoreconf --install				\
+	&& autoconf					\
+	&& libtoolize 	--copy --force --automake 	\
+	&& autoreconf --install				\
+	&& ./configure --prefix=/usr --enable-werror
