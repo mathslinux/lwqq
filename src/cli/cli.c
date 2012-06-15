97d2bc56ea14dec9106c3ea8c62f8e1882310caf
mathslinux <riegamaths@gmail.com>
2012-06-16 04:00:06 +0800

    cli: Add signal handler() so user can press down Ctrl-C to exit program
    Signed-off-by: mathslinux <riegamaths@gmail.com>

--- src/cli/cli.c
+++ src/cli/cli.c
@@ -12,6 +12,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <getopt.h>
+#include <signal.h>
 
 #include "login.h"
 #include "logger.h"
@@ -20,6 +21,8 @@
 #include "msg.h"
 
 #define LWQQ_CLI_VERSION "0.0.1"
+
+static LwqqClient *lc = NULL;
 
 static char vc_image[128];
 static char vc_file[128];
@@ -46,7 +49,7 @@ static char *get_vc()
     return s_strdup(vc);
 }
 
-static LwqqErrorCode cli_login(LwqqClient *lc)
+static LwqqErrorCode cli_login()
 {
     LwqqErrorCode err;
 
@@ -83,7 +86,7 @@ failed:
     return LWQQ_EC_ERROR;
 }
 
-static void cli_logout(LwqqClient *lc)
+static void cli_logout()
 {
     LwqqErrorCode err;
     
@@ -110,10 +113,18 @@ static void usage()
         );
 }
 
+void signal_handler(int signum)
+{
+	if (signum == SIGINT) {
+        cli_logout(lc);
+        lwqq_client_free(lc);
+        exit(0);
+	}
+}
+
 int main(int argc, char *argv[])
 {
     char *qqnumber = NULL, *password = NULL;
-    LwqqClient *lc;
     LwqqErrorCode err;
     int c, e = 0;
 
@@ -121,7 +132,8 @@ int main(int argc, char *argv[])
         usage();
         exit(1);
     }
-    static const struct option long_options[] = {
+    
+    const struct option long_options[] = {
         { "version", 0, 0, 'v' },
         { "help", 0, 0, 'h' },
         { "user", 0, 0, 'u' },
@@ -129,6 +141,9 @@ int main(int argc, char *argv[])
         { 0, 0, 0, 0 }
     };
 
+    /* Lanuch signal handler when user press down Ctrl-C in terminal */
+    signal(SIGINT, signal_handler);
+    
     while ((c = getopt_long(argc, argv, "vhu:p:",
                             long_options, NULL)) != EOF) {
         switch (c) {
@@ -166,7 +181,7 @@ int main(int argc, char *argv[])
     }
 
     /* Login to server */
-    err = cli_login(lc);
+    err = cli_login();
     if (err != LWQQ_EC_OK) {
         lwqq_log(LOG_ERROR, "Login error, exit\n");
         lwqq_client_free(lc);
