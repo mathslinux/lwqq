b9705bcdf7ae909c47e085ec1d162f57fedca34c
mathslinux <riegamaths@gmail.com>
2012-07-12 15:29:14 +0800

    Refactor poll thread
    Signed-off-by: mathslinux <riegamaths@gmail.com>

--- src/cli/cli.c
+++ src/cli/cli.c
@@ -260,38 +260,36 @@ static void *recvmsg_thread(void *list)
 
     /* Need to wrap those code so look like more nice */
     while (1) {
-        sleep(1);
         LwqqRecvMsg *msg;
         pthread_mutex_lock(&l->mutex);
-        if (!SIMPLEQ_EMPTY(&l->head)) {
-            msg = SIMPLEQ_FIRST(&l->head);
-            char * msg_type = msg->msg->msg_type;
-            if (msg_type && strlen(msg_type) > 0) {
-                printf("Receive message type: %s \n", msg->msg->msg_type);
-                if (strcmp(msg_type, MT_MESSAGE) == 0) {
-                    LwqqMsgMessage * m = (LwqqMsgMessage *)(msg->msg);
-                    if (m->content) {
-                        printf("Receive message: %s\n", m->content);
-                    }
-                } else if ( strcmp(msg_type, MT_GROUP_MESSAGE) == 0 ) {
-                    if (msg->msg->message.content) {
-                        printf("Receive group message: %s\n", msg->msg->message.content);
-                    }
-                } else if (strcmp(msg_type, MT_STATUS_CHANGE) == 0) {
-                    if (msg->msg->status.who
-                        && msg->msg->status.status) {
-                        printf("Receive status change: %s - > %s\n", 
-                                msg->msg->status.who,
-                                msg->msg->status.status);
-                    }
-                } else  {
-                    printf("unknow message\n");
-                }
-
-            }
-            SIMPLEQ_REMOVE_HEAD(&l->head, entries);
+        if (SIMPLEQ_EMPTY(&l->head)) {
+            /* No message now, wait 100ms */
+            pthread_mutex_unlock(&l->mutex);
+            usleep(100000);
+            continue;
         }
+        msg = SIMPLEQ_FIRST(&l->head);
+        char *msg_type = msg->msg->msg_type;
+        printf("Receive message type: %s \n", msg->msg->msg_type);
+
+        if (strcmp(msg_type, MT_MESSAGE) == 0) {
+            LwqqMsgMessage *m = (LwqqMsgMessage *)(msg->msg);
+            printf("Receive message: %s\n", m->content);
+        } else if (strcmp(msg_type, MT_GROUP_MESSAGE) == 0) {
+            printf("Receive group message: %s\n", msg->msg->message.content);
+        } else if (strcmp(msg_type, MT_STATUS_CHANGE) == 0) {
+            printf("Receive status change: %s - > %s\n", 
+                   msg->msg->status.who,
+                   msg->msg->status.status);
+        } else {
+            printf("unknow message\n");
+        }
+
+        SIMPLEQ_REMOVE_HEAD(&l->head, entries);
         pthread_mutex_unlock(&l->mutex);
+
+        lwqq_msg_free(msg->msg);
+        s_free(msg);
     }
 
     pthread_exit(NULL);
