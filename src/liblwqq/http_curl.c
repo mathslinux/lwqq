69401f32a21b09baebd37a803e6735dc0aeed1e3
mathslinux <riegamaths@gmail.com>
2012-08-19 16:58:23 +0800

    http: Add missing file http_curl.c
    Signed-off-by: mathslinux <riegamaths@gmail.com>

---
+++ src/liblwqq/http_curl.c
@@ -0,0 +1,12 @@
+/**
+ * @file   http_curl.c
+ * @author mathslinux <riegamaths@gmail.com>
+ * @date   Sun Aug 19 16:46:22 2012
+ * 
+ * @brief  Use asynchronous http translation
+ * 
+ * 
+ */
+
+#ifdef USE_ASYNC_HTTP
+#endif  /* USE_ASYNC_HTTP */
