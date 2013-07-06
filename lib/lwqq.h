6650e118fa6ac1a27dec281005d952a84b94f516
xiehuc <xiehuc@gmail.com>
2013-07-06 13:27:19 +0800

    init version
    Signed-off-by: xiehuc <xiehuc@gmail.com>

---
+++ lib/lwqq.h
@@ -0,0 +1,11 @@
+
+
+#include "type.h"
+#include "info.h"
+#include "login.h"
+#include "msg.h"
+#include "utility.h"
+#include "logger.h"
+#include "async.h"
+#include "smemory.h"
+#include "http.h"
